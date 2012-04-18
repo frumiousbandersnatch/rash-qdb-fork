80ac6441fa3eb0c940d20c173f57cd01f918bb3f
Pasi Kallinen <paxed@alt.org>
2012-04-18 17:47:53 +0300

    Silence a warning

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ date_default_timezone_set($CONFIG['timezone']);
 if (isset($_COOKIE['lastvisit']) && !isset($_SESSION['lastvisit'])) {
     $_SESSION['lastvisit'] = $_COOKIE['lastvisit'];
 }
-mk_cookie('lastvisit', mktime());
+mk_cookie('lastvisit', time());
 
 set_voteip($CONFIG['secret_salt']);
 
